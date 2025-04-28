89e0c4aba4d1228c284c4581c604cfca93fb4c33
rodohub09 <superpro757@gmail.com>
2025-04-28 13:19:49 -0400

    Cambio en el método Main dados los cambios en constructor de Expendedor

--- Tarea 1/src/main/java/Main.java
+++ Tarea 1/src/main/java/Main.java
@@ -1,27 +1,36 @@
 
 public class Main {
     public static void main(String[] args) {
-        Expendedor exp = new Expendedor(1,300);
+        Expendedor exp = new Expendedor(1);
         Moneda m = null;
-        Comprador c=null;
-        m = new Moneda1000();
-        try{
-            c = new Comprador(m,Productos.CocaCola,exp);
+        Comprador c;
+
+        for(int i=1; i<=2;i++) {
+            c=null;
+            m = new Moneda100();
+            try {
+                c = new Comprador(m, Productos.CocaCola, exp);
+            }
+            catch (Exception e) {
+                if (e instanceof PagoIncorrectoException) {
+                    System.out.println("La moneda ingresada no es válida");
+                    break;
+                }
+                else if (e instanceof PagoInsuficienteException) {
+                    System.out.println("Saldo insuficiente, compra cancelada");
+                    break;
+                }
+                else if (e instanceof NoHayProductoException) {
+                    System.out.println("Producto no disponible, compra cancelada");
+                    break;
+                }
+            }
+
+            if(c!=null)
+                System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
         }
-        catch (Exception e){
-            if (e instanceof PagoIncorrectoException)
-                System.out.println("La moneda ingresada no es válida");
 
-            else if (e instanceof PagoInsuficienteException)
-                System.out.println("Saldo insuficiente, compra cancelada");
 
-            else if (e instanceof NoHayProductoException)
-                System.out.println("Producto no disponible, compra cancelada");
-
-        }
-
-        if(c!=null)
-            System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
 
         Moneda n = new Moneda500();
 
