ed68b29e903f21ad98432cca869917b99b03e400
mihn <martin.henrnav@gmail.com>
2025-04-28 14:27:54 -0400

    Actualizacion de Comprador con Exceptions

--- Tarea 1/src/main/java/Comprador.java
+++ Tarea 1/src/main/java/Comprador.java
@@ -2,7 +2,7 @@ public class Comprador {
     private int vuelto = 0;
     private String sonido;
 
-    public Comprador(Moneda m, Productos p, Expendedor exp) {
+    public Comprador(Moneda m, Productos p, Expendedor exp) throws Exception{
         Producto product = exp.comprarProducto(m, p);
         if (product != null)
             sonido = product.consumir();
