e7e1aa823686aec8dff71d80b35a010e313b6c26
JP3dr3 <jpedreros2024@udec.cl>
2025-05-01 23:28:14 -0400

    Comentarios en todas las excepciones con sus respectivos JavaDoc.

--- Tarea 1/src/main/java/PagoIncorrectoException.java
+++ Tarea 1/src/main/java/PagoIncorrectoException.java
@@ -1,4 +1,12 @@
+/**
+ * Esta excepcion({@link Exception}) es ocupada, cuando el {@link Comprador} intenta comprar un {@link Producto} sin una {@link Moneda} valida.(null)
+ * */
+
 public class PagoIncorrectoException extends Exception{
+
+    /**
+     * Constructor de la excepcion, ocuupando el constructor de su superclase {@link Exception} .
+     * */
     public PagoIncorrectoException(){
         super();
     }
