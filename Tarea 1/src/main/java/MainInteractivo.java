50fbf2a235678a1f068e7edf5e4b29feb3b6434e
mihn <martin.henrnav@gmail.com>
2025-05-01 20:58:00 -0400

    Actualizacion del main interactivo.

--- Tarea 1/src/main/java/MainInteractivo.java
+++ Tarea 1/src/main/java/MainInteractivo.java
@@ -5,42 +5,34 @@ public class MainInteractivo{
     public static void main(String[] args) {
         Expendedor exp = new Expendedor(5);
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<Moneda> monedas = new ArrayList<>();
-        monedas.add(new Moneda100());
-        monedas.add(new Moneda500());
-        monedas.add(new Moneda1000());
-        monedas.sort(Moneda::compareTo);
-
-        Productos producto = null;
-
         boolean comprar = true;
-        int opcion = -1;
-        String compra;
-        int vuelto;
 
+
+        label:
         while (comprar) {
             System.out.println("-------- Bienvenido --------\n\n");
+
+            ArrayList<Moneda> monedas = new ArrayList<>();
+            monedas.add(new Moneda100());
+            monedas.add(new Moneda500());
+            monedas.add(new Moneda1000());
+            monedas.sort(Moneda::compareTo);
 
             for (int i = 0; i < monedas.size(); i++) {
                 System.out.println((i + 1) + ". Moneda de " + monedas.get(i).getValor());
             }
             System.out.print("Ingresa una moneda: ");
 
-            Moneda moneda = null;
+            Moneda moneda;
             try {
-                opcion = scanner.nextInt() - 1;
+                int opcion = scanner.nextInt();
 
-                if (opcion < 0 || opcion >= monedas.size()) {
-                    throw new PagoIncorrectoException();
+                switch (opcion) {
+                    case 1 -> moneda = new Moneda100();
+                    case 2 -> moneda = new Moneda500();
+                    case 3 -> moneda = new Moneda1000();
+                    default -> throw new PagoIncorrectoException();
                 }
-
-                int valor = monedas.get(opcion).getValor();
-
-                if (valor == 100) moneda = new Moneda100();
-                else if (valor == 500) moneda = new Moneda500();
-                else if (valor == 1000) moneda = new Moneda1000();
-                else throw new PagoIncorrectoException();
 
                 System.out.println("Se ingresa una moneda de " + moneda.getValor() + "...\n\n");
 
@@ -50,7 +42,9 @@ public class MainInteractivo{
                 System.out.println("3. Fanta");
                 System.out.println("4. Snickers");
                 System.out.println("5. Super8");
+
                 int select = scanner.nextInt();
+                Productos producto = null;
 
                 for (Productos p : Productos.values()) {
                     if (select == p.getOpcion()) {
@@ -60,37 +54,39 @@ public class MainInteractivo{
                 }
 
                 Comprador comprador = new Comprador(moneda, producto, exp);
-                compra = comprador.queConsumiste();
-                vuelto = comprador.cuantoVuelto();
+                String compra = comprador.queConsumiste();
+                int vuelto = comprador.cuantoVuelto();
                 System.out.println("Compré " + compra + " y tuve " + vuelto + " de vuelto.");
 
-                System.out.println("¿Desea realizar otra compra?");
-                System.out.println("1. Sí\n2. No");
-                int nuevaCompra = scanner.nextInt();
-                if (nuevaCompra == 1)
-                    continue;
-                else {
-                    System.out.println("Muchas gracias por su preferencia, vuelva pronto!!\n\n");
-                    comprar = false;
-                    break;
-                }
 
             } catch (Exception e) {
-                if (e instanceof PagoIncorrectoException) {
-                    System.out.println("La moneda ingresada no es válida");
-                    System.exit(0);
-                    break;
-                }
-                else if (e instanceof PagoInsuficienteException) {
-                    System.out.println("Saldo insuficiente, compra cancelada");
-                    break;
-                }
-                else if (e instanceof NoHayProductoException) {
-                    System.out.println("Producto no disponible, compra cancelada");
-                    break;
+                switch (e) {
+                    case PagoIncorrectoException pagoIncorrectoException:
+                        System.out.println("La moneda ingresada no es válida\n");
+                        break;
+                    case PagoInsuficienteException pagoInsuficienteException:
+                        System.out.println("Saldo insuficiente, compra cancelada\n");
+                        break;
+                    case NoHayProductoException noHayProductoException:
+                        System.out.println("Producto no disponible, compra cancelada\n");
+                        break;
+                    default:
+                        break;
                 }
             }
+
+            System.out.println("¿Desea realizar otra compra?");
+            System.out.println("1. Sí\n2. No");
+            int nuevaCompra = scanner.nextInt();
+
+            while (nuevaCompra != 1 && nuevaCompra != 2) {
+                System.out.println("Opción no válida, intente nuevamente.\n");
+                nuevaCompra = scanner.nextInt();
+            }
+
+            if (nuevaCompra == 2) comprar = false;
         }
+        System.out.println("Muchas gracias por su preferencia, vuelva pronto!!\n\n");
         scanner.close();
         System.exit(0);
     }
